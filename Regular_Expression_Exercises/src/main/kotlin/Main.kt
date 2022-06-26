5de7e0b5b6ea38b78e7a368bea61e823bffd3748
Kayevo <kayevo@protonmail.com>
2022-06-26 14:42:01 -0300

    feat: ignore sensitive case when verify patterns on regular expressions.

--- Regular_Expression_Exercises/src/main/kotlin/Main.kt
+++ Regular_Expression_Exercises/src/main/kotlin/Main.kt
@@ -3,12 +3,20 @@ fun main(args: Array<String>) {
     val valueA = "bacross"
     println(initWithA(valueA));
 
-    val valueB = "Across"
+    val valueB = "across"
     println(initWithA(valueB));
 }
 
 fun initWithA(_values: String): Boolean{
     var pattern = Regex("^a|^A");
+    var result = pattern.containsMatchIn(_values);
+
+    return  result
+}
+
+// ignore sensitive case
+fun initWithANoSensCase(_values: String): Boolean{
+    var pattern = "^A".toRegex(RegexOption.IGNORE_CASE)
     var result = pattern.containsMatchIn(_values);
 
     return  result
